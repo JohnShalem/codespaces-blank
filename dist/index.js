ca6e14208e9ccfff63964b95b0ea877c1bf60a3c
johnshalemt777 <78728268+JohnShalem@users.noreply.github.com>
2023-11-21 13:26:41 +0000

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function helloWorld() {
+    const element = document.getElementById("hello-world");
+    if (element != null)
+        element.textContent = "Hello, World!";
+}
+helloWorld();
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,8 @@
+function helloWorld() {
+    const element= document.getElementById("hello-world");
+    
+  if(element!=null)
+      element.textContent = "Hello, World!";
+    
+  }
+  helloWorld();
