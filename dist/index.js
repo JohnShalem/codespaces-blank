27bba7d44f27fdc9981bf2662d3a12dd3ba55f60
johnshalemt777 <78728268+JohnShalem@users.noreply.github.com>
2023-11-24 07:35:10 +0000

    commit#5

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi';
+import { mainnet, arbitrum } from 'viem/chains';
+// 1. Define constants
+const projectId = 'YOUR_PROJECT_ID';
+// 2. Create wagmiConfig
+const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+};
+const chains = [mainnet, arbitrum];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+// 3. Create modal
+const modal = createWeb3Modal({ wagmiConfig, projectId, chains });
+function connect() {
+    modal.open();
+}
+const btnEl = document.getElementById('btn');
+btnEl === null || btnEl === void 0 ? void 0 : btnEl.addEventListener('click', connect);
+//# sourceMappingURL=index.js.map
