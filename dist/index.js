f93d8b4a0b70b868886afd76304fe50dbae27588
johnshalemt777 <78728268+JohnShalem@users.noreply.github.com>
2023-11-24 07:25:24 +0000

    added web3modal commit#4

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function helloWorld() {
-    const element = document.getElementById("hello-world");
-    if (element != null)
-        element.textContent = "Hello, World!";
-}
-helloWorld();
-//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
-function helloWorld() {
-    const element= document.getElementById("hello-world");
-    
-  if(element!=null)
-      element.textContent = "Hello, World!";
-    
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
+
+import { mainnet, arbitrum } from 'viem/chains'
+
+// 1. Define constants
+const projectId = 'YOUR_PROJECT_ID'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+const modal = createWeb3Modal({ wagmiConfig, projectId, chains })
+
+
+function connect() {
+      modal.open()   
   }
-  helloWorld();
+
+  const btnEl = document.getElementById('btn');
+
+btnEl?.addEventListener('click', connect)
